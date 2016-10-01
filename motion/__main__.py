9906c1840067ffd7ab6e6802379dac0d08679362
Fabio Mendes <fabiojmendes@gmail.com>
2016-10-01 10:36:06 -0400

    Width & Height should be numbers

--- motion/__main__.py
+++ motion/__main__.py
@@ -2,11 +2,12 @@ import main
 from argparse import ArgumentParser
 
 def size_tuple(size_str):
-	return tuple(size_str.split('x'))
+	w, h = size_str.split('x')
+	return tuple(int(w), int(h))
 
 if __name__ == '__main__':
 	parser = ArgumentParser(description='Process some integers.')
-	parser.add_argument('--fps', type=float, default=10)
+	parser.add_argument('--fps', type=int, default=10)
 	parser.add_argument('--size', type=size_tuple, default=(1280,720), help='The frame size in WxH')
 
 	args = parser.parse_args()

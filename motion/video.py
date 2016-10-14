f503c56b42f88307e6375ad1aa754ecaa45e1028
Fabio Mendes <fabiojmendes@gmail.com>
2016-10-14 18:47:18 -0400

    Player improvements & Video Duration

--- motion/video.py
+++ motion/video.py
@@ -1,35 +1,44 @@
 import cv2
 import os
+from datetime import datetime
 
 from threading import Thread
 from queue import Queue
 
 QueueFinished = object()
 
+filename_tmpl = "video-{:%Y%m%d-%H%M%S}-{:03d}.{}"
+
 class VideoWriter():
     def __init__(self, size, fps, date, codec, extension):
-        self.filename = ".video-{:%Y%m%d-%H%M%S}.{}".format(date, extension)
-        fourcc = cv2.VideoWriter_fourcc(*codec) if codec else 0
-        self.video = cv2.VideoWriter(self.filename, fourcc, fps, size)
+        self.date = date
+        self.extension = extension
+        self.fourcc = cv2.VideoWriter_fourcc(*codec) if codec else 0
+        self.fps = fps
+        self.size = size
         self.queue = Queue(maxsize = 32)
-        self.thread = Thread(target=self.run)
-        self.thread.start()
+        thread = Thread(target=self.run)
+        thread.start()
 
     def write(self, frame):
         self.queue.put(frame)
 
     def run(self):
-        print('Starting new video File [{}]'.format(self.filename[1:]))
+        filename = '.' + filename_tmpl.format(self.date, 0, self.extension)
+        print('Starting new video File [{}]'.format(filename))
+        video = cv2.VideoWriter(filename, self.fourcc, self.fps, self.size)
         while True:
             f = self.queue.get()
             if f is QueueFinished:
+                self.queue.task_done()
                 break
-            self.video.write(f)
+            video.write(f)
             self.queue.task_done()
-        self.queue.task_done()
-        print('Releasing resources for [{}]'.format(self.filename[1:]))
-        self.video.release()
-        os.rename(self.filename, self.filename[1:])
+        duration = (datetime.now() - self.date).total_seconds()
+        new_filename = filename_tmpl.format(self.date, round(duration), self.extension)
+        print('Releasing resources for [{}] and renaming it to [{}]'.format(filename, new_filename))
+        video.release()
+        os.rename(filename, new_filename)
 
     def close(self, wait=False):
         self.queue.put(QueueFinished)
--- web/server.py
+++ web/server.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    values = ''
+    for f in os.listdir('.'):
+        values += '{}\n'.format(f)
+    return "Hello World!\n" + values
 
 if __name__ == "__main__":
     app.run()

3ae85840a8afcc4a127cb27123a0d65dae29f05c
Fabio Mendes <fabiojmendes@gmail.com>
2016-10-27 19:36:08 -0400

    asyncio-notifier

--- motionweb/notifier.py
+++ motionweb/notifier.py
@@ -6,10 +6,31 @@ import redis
 import types
 import signal
 from motionweb import utils
+from concurrent.futures import ThreadPoolExecutor
 
 listeners = set()
 
+msg_queue = asyncio.Queue()
+END_OF_QUEUE = object()
+
+executor = ThreadPoolExecutor(1)
+
+def queue_listener():
+    # asyncio.set_event_loop(loop)
+    rd = redis.StrictRedis(decode_responses=True)
+    ps = rd.pubsub(ignore_subscribe_messages=True)
+    ps.subscribe('video:new')
+    while True:
+        msg = ps.get_message()
+        if msg:
+            print('Message Received', msg)
+            # new_video_handler(msg)
+            # new_video_handler(msg)
+            msg_queue.put_nowait(msg)
+
+
 def new_video_handler(msg):
+    print(msg)
     filename = msg['data']
     print('Data received: {}'.format(filename))
     video = utils.video_to_dict(filename)
@@ -18,16 +39,16 @@ def new_video_handler(msg):
     for l in listeners:
         asyncio.ensure_future(l.send(obj))
 
-async def setup_redis(loop, stop_app):
-    rd = redis.StrictRedis(decode_responses=True)
-    ps = rd.pubsub(ignore_subscribe_messages=True)
-    ps.subscribe('video:new')
-    listen = types.coroutine(ps.listen)
-    while not stop_app.is_set():
-        msg = ps.get_message()
-        if msg:
-            new_video_handler(msg)
-        await asyncio.sleep(0.01)
+async def setup_redis(loop):
+    loop.run_in_executor(executor, queue_listener)
+    print('Redis key store ready')
+    while True:
+        msg = await msg_queue.get()
+        print('Message got', msg)
+        if msg is END_OF_QUEUE:
+            break
+        new_video_handler(msg)
+        # await asyncio.sleep(0.01)
 
     # async for msg in generator():
     # while True:
@@ -48,21 +69,23 @@ def run():
     stop_app = asyncio.Event()
 
     loop = asyncio.get_event_loop()
-    loop.add_signal_handler(signal.SIGINT, stop_running, loop, stop_app)
-    loop.add_signal_handler(signal.SIGTERM, stop_running, loop, stop_app)
+    loop.add_signal_handler(signal.SIGINT, stop_running, loop)
+    loop.add_signal_handler(signal.SIGTERM, stop_running, loop)
 
     server_config = websockets.serve(handler, 'localhost', 8765)
     ws_server = loop.run_until_complete(server_config)
-    print('After run until complete', ws_server, server_config)
-    loop.run_until_complete(setup_redis(loop, stop_app))
+    loop.run_until_complete(setup_redis(loop))
 
-    ws_server.close()
-    loop.run_until_complete(ws_server.wait_closed())
+    try:
+        loop.run_forever()
+    finally:
+        ws_server.close()
+        loop.run_until_complete(ws_server.wait_closed())
+
+def stop_running(loop):
+    print('Stopping event loop')
+    executor.shutdown(wait=True)
     loop.close()
-
-def stop_running(loop, stop_app):
-    print('Stopping event loop', loop)
-    stop_app.set()
 
 def start():
     proc = multiprocessing.Process(target=run)
